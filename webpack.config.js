1c0c12338c329efc07d6c60b367b8c6d1d9af143
Florian Calabrese florian.calabrese@gmail.com <florian.calabrese@gmail.com>
2023-03-27 21:29:40 +0200

    bug fixed postgresql

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
     })
   ],
   performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
+  hints: false,
+  maxEntrypointSize: 512000,
+  maxAssetSize: 512000
 }
 }
